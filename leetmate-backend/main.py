0da80504a3aa607c8bc9ff1d38099a2b872be67c
Hardeep Singh Sodha <hardeepsinghsodha7@gmail.com>
2026-07-07 14:46:22 +0530

    fix trailing slash and CORS

--- leetmate-backend/main.py
+++ leetmate-backend/main.py
@@ -14,9 +14,7 @@ init_db()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[    "https://your-vercel-url.vercel.app",
-                       "http://127.0.0.1:5500",
-                       "http://localhost:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
